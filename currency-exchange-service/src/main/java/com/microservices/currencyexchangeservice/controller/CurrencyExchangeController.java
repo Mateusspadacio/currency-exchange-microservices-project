d30adf11744c9a8ecb242d1d6260779f329efbeb
Mateusspadacio <mateusspadacio@gmail.com>
2019-04-29 16:55:47 -0300

    set port removed from controller

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,7 +3,6 @@ package com.microservices.currencyexchangeservice.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +17,6 @@ public class CurrencyExchangeController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	Environment env;
-	
-	@Autowired
 	private CurrencyExchangeService service;
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
@@ -28,7 +24,6 @@ public class CurrencyExchangeController {
 			@PathVariable String to) {
 		
 		ExchangeValue exchangeValue = service.findCurrency(from, to);
-		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		
 		logger.info("{}", exchangeValue);
 		return ResponseEntity.ok(exchangeValue);
