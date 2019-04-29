0777c3f35102be45b0cb72e7d555c2af029d5012
Mateusspadacio <mateusspadacio@gmail.com>
2019-04-29 16:56:09 -0300

    set port added in service

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/service/CurrencyExchangeService.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/service/CurrencyExchangeService.java
@@ -1,6 +1,7 @@
 package com.microservices.currencyexchangeservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.microservices.currencyexchangeservice.exception.CurrencyExchangeNotFound;
@@ -9,17 +10,21 @@ import com.microservices.currencyexchangeservice.repository.CurrencyExchangeRepo
 
 @Service
 public class CurrencyExchangeService {
+	
+	@Autowired
+	Environment env;
 
 	@Autowired
 	private CurrencyExchangeRepository repository;
 
 	public ExchangeValue findCurrency(String from, String to) {
-		ExchangeValue exchangeValue = repository.findByFromAndTo(from.toUpperCase(), to.toUpperCase());
+		ExchangeValue exchangeValue = repository.findByFromIgnoreCaseAndToIgnoreCase(from, to);
 		
 		if (exchangeValue == null) {
 			throw new CurrencyExchangeNotFound(String.format("currency exchange not found from %s to %s", from, to));
 		}
 		
+		exchangeValue.setPort(Integer.parseInt(env.getProperty("server.port")));
 		return exchangeValue;
 	}
 
